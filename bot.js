f52dcb1c6e959c3436278ab321171125d1835ae3
xlizer1 <mustafa_142@mail.ru>
2023-03-19 12:46:53 +0300

    finished bot configuration with adding just the play command

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 
 const { TOKEN, CLIENTID, GUILDID } = process.env;
 
-const client = new Client({
+const bot = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers,
@@ -36,7 +36,7 @@ const client = new Client({
 
 const commands = [];
 
-client.commands = new Collection();
+bot.commands = new Collection();
 
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
@@ -47,13 +47,13 @@ for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
   commands.push(command.data.toJSON());
-  client.commands.set(command.data.name, command);
+  bot.commands.set(command.data.name, command);
 }
 
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 // and deploy your commands!
-client.once("ready", () => {
+bot.once("ready", () => {
   (async () => {
     try {
       console.log(
@@ -76,10 +76,10 @@ client.once("ready", () => {
   })();
 });
 
-client.on(Events.InteractionCreate, async (interaction) => {
+bot.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  const command = client.commands.get(interaction.commandName);
+  const command = bot.commands.get(interaction.commandName);
 
   if (!command) return;
 
@@ -102,4 +102,4 @@ client.on(Events.InteractionCreate, async (interaction) => {
   }
 });
 
-client.login(TOKEN);
+bot.login(TOKEN);
